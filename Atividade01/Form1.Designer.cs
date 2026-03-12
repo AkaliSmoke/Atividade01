14ff551161269a341788b5c2c6c8acf8960d1cc7
Kaio <kaiomascarenhas241@gmail.com>
2026-03-11 22:26:28 -0300

    Initial commit

---
+++ Atividade01/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Atividade01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCampo = new System.Windows.Forms.TextBox();
+            this.lbResultado = new System.Windows.Forms.Label();
+            this.btnIdade = new System.Windows.Forms.Button();
+            this.btnTabuada = new System.Windows.Forms.Button();
+            this.btnVerificarLetra = new System.Windows.Forms.Button();
+            this.btnAprendendo = new System.Windows.Forms.Button();
+            this.btnContar10 = new System.Windows.Forms.Button();
+            this.btnJogar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCampo
+            // 
+            this.txtCampo.Location = new System.Drawing.Point(59, 42);
+            this.txtCampo.Name = "txtCampo";
+            this.txtCampo.Size = new System.Drawing.Size(275, 20);
+            this.txtCampo.TabIndex = 0;
+            this.txtCampo.TextChanged += new System.EventHandler(this.txtCampo_TextChanged);
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.AutoSize = true;
+            this.lbResultado.Location = new System.Drawing.Point(59, 69);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(55, 13);
+            this.lbResultado.TabIndex = 1;
+            this.lbResultado.Text = "Resultado";
+            // 
+            // btnIdade
+            // 
+            this.btnIdade.Location = new System.Drawing.Point(56, 139);
+            this.btnIdade.Name = "btnIdade";
+            this.btnIdade.Size = new System.Drawing.Size(127, 55);
+            this.btnIdade.TabIndex = 2;
+            this.btnIdade.Text = "Calcular Idade";
+            this.btnIdade.UseVisualStyleBackColor = true;
+            this.btnIdade.Click += new System.EventHandler(this.btnIdade_Click);
+            // 
+            // btnTabuada
+            // 
+            this.btnTabuada.Location = new System.Drawing.Point(207, 139);
+            this.btnTabuada.Name = "btnTabuada";
+            this.btnTabuada.Size = new System.Drawing.Size(127, 55);
+            this.btnTabuada.TabIndex = 3;
+            this.btnTabuada.Text = "Calcular Tabuada";
+            this.btnTabuada.UseVisualStyleBackColor = true;
+            this.btnTabuada.Click += new System.EventHandler(this.btnTabuada_Click);
+            // 
+            // btnVerificarLetra
+            // 
+            this.btnVerificarLetra.Location = new System.Drawing.Point(56, 201);
+            this.btnVerificarLetra.Name = "btnVerificarLetra";
+            this.btnVerificarLetra.Size = new System.Drawing.Size(127, 51);
+            this.btnVerificarLetra.TabIndex = 4;
+            this.btnVerificarLetra.Text = "Verificar Letra";
+            this.btnVerificarLetra.UseVisualStyleBackColor = true;
+            // 
+            // btnAprendendo
+            // 
+            this.btnAprendendo.Location = new System.Drawing.Point(207, 201);
+            this.btnAprendendo.Name = "btnAprendendo";
+            this.btnAprendendo.Size = new System.Drawing.Size(127, 51);
+            this.btnAprendendo.TabIndex = 5;
+            this.btnAprendendo.Text = "Estou Aprendendo";
+            this.btnAprendendo.UseVisualStyleBackColor = true;
+            this.btnAprendendo.Click += new System.EventHandler(this.btnAprendendo_Click);
+            // 
+            // btnContar10
+            // 
+            this.btnContar10.Location = new System.Drawing.Point(56, 258);
+            this.btnContar10.Name = "btnContar10";
+            this.btnContar10.Size = new System.Drawing.Size(127, 51);
+            this.btnContar10.TabIndex = 6;
+            this.btnContar10.Text = "Contar até 10";
+            this.btnContar10.UseVisualStyleBackColor = true;
+            this.btnContar10.Click += new System.EventHandler(this.btnContar10_Click);
+            // 
+            // btnJogar
+            // 
+            this.btnJogar.Location = new System.Drawing.Point(207, 258);
+            this.btnJogar.Name = "btnJogar";
+            this.btnJogar.Size = new System.Drawing.Size(127, 51);
+            this.btnJogar.TabIndex = 7;
+            this.btnJogar.Text = "Mini Jogo";
+            this.btnJogar.UseVisualStyleBackColor = true;
+            this.btnJogar.Click += new System.EventHandler(this.btnJogar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 371);
+            this.Controls.Add(this.btnJogar);
+            this.Controls.Add(this.btnContar10);
+            this.Controls.Add(this.btnAprendendo);
+            this.Controls.Add(this.btnVerificarLetra);
+            this.Controls.Add(this.btnTabuada);
+            this.Controls.Add(this.btnIdade);
+            this.Controls.Add(this.lbResultado);
+            this.Controls.Add(this.txtCampo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCampo;
+        private System.Windows.Forms.Label lbResultado;
+        private System.Windows.Forms.Button btnIdade;
+        private System.Windows.Forms.Button btnTabuada;
+        private System.Windows.Forms.Button btnVerificarLetra;
+        private System.Windows.Forms.Button btnAprendendo;
+        private System.Windows.Forms.Button btnContar10;
+        private System.Windows.Forms.Button btnJogar;
+    }
+}
+
---
+++ Atividade01/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIdade_Click(object sender, EventArgs e)
+        {
+            if (txtCampo.Text != string.Empty)
+            {
+
+            
+                int idadeUsuario = int.Parse(txtCampo.Text);
+            
+                // se a idade do usuario for maior ou igual a 18
+                // resultado: Você é maior de idade
+
+                if (idadeUsuario >= 18 )
+                {
+                    //MessageBox.Show("Você é MAIOR de idade");
+                    lbResultado.Text = "Você é MAIOR de idade";
+                } else
+                {
+                    //MessageBox.Show("Você é MENOR de idade");
+                    lbResultado.Text = "Você é MENOR de idade";
+                }
+            } else
+            {
+                MessageBox.Show("Por favor insira a sua idade",
+
+                    "mensagem de aviso");
+            }
+        }
+
+        private void txtCampo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTabuada_Click(object sender, EventArgs e)
+        {
+            int numeroUsuario = int.Parse(txtCampo.Text);
+            int resultado;
+            string respostaFinal = "";
+
+            //para variavel inicial, condição, calculo
+
+            for (int contador = 1; contador <= 10; contador += 1)
+            {
+                resultado = numeroUsuario * contador;
+                respostaFinal = respostaFinal + numeroUsuario + "x" + contador + "=" + resultado + "\n";
+            }
+
+            MessageBox.Show(respostaFinal, "Tabuada do " + numeroUsuario);
+        }
+
+        private void btnJogar_Click(object sender, EventArgs e)
+        {
+            {
+                // etapa 01 - gerar um numero aleatorio
+                Random aleatorio = new Random();
+                int numeroSorteado = aleatorio.Next(1, 11);
+
+                string numeroSecretoSorteado = lbResultado.Text;
+                string numeroDigitadoUsuario = txtCampo.Text;
+
+                if (numeroSecretoSorteado == "Resultado")
+                {
+                    lbResultado.Text = numeroSecretoSorteado.ToString();
+                    numeroSecretoSorteado = numeroSorteado.ToString();
+                    lbResultado.Visible = false;
+                }
+                // etapa 02 - pegar valor do usuario e validar se é igual ao numero sorteado
+                if (numeroSecretoSorteado == numeroDigitadoUsuario)
+                {
+                    MessageBox.Show("Parabéns você ganhou!!");
+                }
+                // etapa 03 - validar se o numero é maior ou menor que o numero sorteado
+                if (int.Parse(numeroDigitadoUsuario) > int.Parse(numeroSecretoSorteado))
+                {
+                    MessageBox.Show("O numero que voce digitou é MAIOR que o valor secreto");
+                }
+                // etapa 04 - validar se o numero é menor que o numero sorteado
+                if (int.Parse(numeroDigitadoUsuario) < int.Parse(numeroSecretoSorteado))
+                {
+                    MessageBox.Show("O numero que você digitou é MENOR que o valor secreto");
+                }
+            }
+        }
+
+        private void btnAprendendo_Click(object sender, EventArgs e)
+        {
+            
+                int qntRepeticao = int.Parse(txtCampo.Text);
+                int contador = 1;
+                //enquanto
+                while (contador <= qntRepeticao)
+                {
+                MessageBox.Show("Estou aprendendo laço de repetição: x" + contador);
+
+                contador++;
+                }           
+        }
+
+        private void btnContar10_Click(object sender, EventArgs e)
+        {
+            //break
+            //continue
+            int numeroUsuario = 7;
+            int contador = 0;
+
+            while (contador < 10)
+            {
+                contador++; //11
+
+                if (contador == numeroUsuario)
+                {
+                    // continue da 'um pulo' no código, ele faz o
+                    // loop voltar para o começo e ignora o resto do laço
+                    // continue;
+
+                    // break finaliza a execução do loop todo, diferente do continue que
+                    // vai rodar até finalizar o laço o break força o laço a ser finalizar
+                    break;
+                }
+                MessageBox.Show("Conta" + contador);
+            }
+        }
+    }
+}
